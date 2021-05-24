dbcccc7f2b9b66f76400e6c676bf556615217d26
孙宇辰 <591610735@qq.com>
2021-05-24 16:28:24 +0800

    团队介绍v1.1

--- pages/lvruimin/lvruimin.js
+++ pages/lvruimin/lvruimin.js
@@ -5,62 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      datalist: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad:function(options)
+  {
+    this.getData();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getData(){
+    let that = this
+    wx.cloud.database().collection("logs").where({
+      id: 18074316
+    }).get({
+      success(res) {
+        console.log("请求成功", res)
+        that.setData({
+          datalist: res.data
+        })
+      },
+      fail(res){
+        console.log("请求失败", res)
+      }
+    })
   }
 })
